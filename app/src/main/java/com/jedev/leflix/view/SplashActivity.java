36278b79174b3013a0dd7b0c9fd3b60c303a5f58
Gerson Ferreira <gerson10caicorn@hotmail.com>
2019-05-09 00:15:43 -0300

    Implementação da Tela Splash com cor padrão(verde)

---
+++ app/src/main/java/com/jedev/leflix/view/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.jedev.leflix.view;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.jedev.leflix.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int TEMPO_SPLASH = 5000; // 5 segundos tela splash
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        // Esconde tanto a barra de navegação e a barra de status .
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        getSupportActionBar().hide(); // esconde actionbar
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TEMPO_SPLASH);
+    }
+}
