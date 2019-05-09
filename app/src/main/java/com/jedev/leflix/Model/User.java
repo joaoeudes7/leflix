d282f6ccb59c5b0c03db8a770a27da2706b1ca59
João Eudes Lima <joaoeudes7@gmail.com>
2019-05-09 15:26:39 -0300

    fix class User

--- app/src/main/java/com/jedev/leflix/Model/User.java
+++ app/src/main/java/com/jedev/leflix/Model/User.java
@@ -18,8 +18,13 @@ public class User {
 
     }
 
-    public User(String email, String password) {
+    public User(String name, String email, String password) {
         this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
