d0de2fd5c0b0003dd976a199004ea54a73b06591
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-05-16 14:40:53 -0300

    tela de pesquisa

--- app/src/main/java/com/jedev/leflix/view/HomeActivity.java
+++ app/src/main/java/com/jedev/leflix/view/HomeActivity.java
@@ -1,15 +1,25 @@
 package com.jedev.leflix.view;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jedev.leflix.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private EditText search;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        search = findViewById(R.id.seach);
+
+        Intent intent = new Intent();
+        intent.putExtra("value", search.getText().toString());
+
     }
 }
