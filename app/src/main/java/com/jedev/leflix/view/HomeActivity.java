d7690a335dca308163d85f7424cc2d33fe567a96
Jamerson macedo <jamerson_macedo@hotmail.com>
2019-05-17 02:16:19 -0300

    tela de pesquisa .

--- app/src/main/java/com/jedev/leflix/view/HomeActivity.java
+++ app/src/main/java/com/jedev/leflix/view/HomeActivity.java
@@ -2,24 +2,56 @@ package com.jedev.leflix.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jedev.leflix.R;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class HomeActivity extends AppCompatActivity {
     private EditText search;
+    MaterialSearchView materialSearchView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        search = findViewById(R.id.seach);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent intent = new Intent();
         intent.putExtra("value", search.getText().toString());
+        materialSearchView = findViewById(R.id.searchview);
+        materialSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        materialSearchView.setMenuItem(menuItem);
+        return super.onCreateOptionsMenu(menu);
+
 
     }
 }
+
