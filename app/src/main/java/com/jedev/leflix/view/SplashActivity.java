0ebd55cb83536d13dad2f24355601901a9b92882
João Eudes Lima <joaoeudes7@gmail.com>
2019-05-30 15:29:20 -0300

    Fix validation of login

--- app/src/main/java/com/jedev/leflix/view/SplashActivity.java
+++ app/src/main/java/com/jedev/leflix/view/SplashActivity.java
@@ -37,7 +37,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (auth != null) {
+                if (user != null) {
                     Intent i = new Intent(SplashActivity.this, HomeActivity.class);
                     startActivity(i);
                     finish();
