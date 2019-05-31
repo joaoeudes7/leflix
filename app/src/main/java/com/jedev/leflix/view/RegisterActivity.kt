c6245e87c93d1c70354b25aabfb78ca689eb63a1
João Eudes Lima <joaoeudes7@gmail.com>
2019-05-31 20:34:37 -0300

    Rename xml activity

--- app/src/main/java/com/jedev/leflix/view/RegisterActivity.kt
+++ app/src/main/java/com/jedev/leflix/view/RegisterActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import com.jedev.leflix.R
 import com.jedev.leflix.controllers.RegisterController
-import kotlinx.android.synthetic.main.register_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,7 +14,7 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_main)
+        setContentView(R.layout.activity_register)
 
         btn_login.setOnClickListener {
             controller.openLogin()
