4d8ddc9bd46ea8928f9eac9694ac32f4197ab7e2
João Eudes Lima <joaoeudes7@gmail.com>
2019-06-21 10:44:19 -0300

    Rename Activity

--- app/src/main/java/com/jedev/leflix/view/NewReadingActivity.kt
+++ app/src/main/java/com/jedev/leflix/view/NewReadingActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jedev.leflix.R
 
-class NewReading : AppCompatActivity() {
+class NewReadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/jedev/leflix/view/ResultsBooksActivity.kt
+++ app/src/main/java/com/jedev/leflix/view/ResultsBooksActivity.kt
@@ -5,7 +5,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jedev.leflix.R
-import com.jedev.leflix.adapter.GoogleBookAdapter
+import com.jedev.leflix.adapter.BookAdapter
+import com.jedev.leflix.model.Book
 import com.jedev.leflix.service.api.GoogleBookService
 import com.jedev.leflix.service.api.entities.Volume
 import kotlinx.android.synthetic.main.activity_results_books.*
@@ -13,13 +14,13 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ResultsBooks : AppCompatActivity() {
+class ResultsBooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results_books)
 
-        setupRecycleView()
+        setupRecyclerView()
 
         intent.extras?.getString("query")?.let { query ->
                 title = "Resultados de '$query''"
@@ -27,20 +28,21 @@ class ResultsBooks : AppCompatActivity() {
                 GoogleBookService().searchBook(query, 20).enqueue(object : Callback<Volume?> {
                     override fun onResponse(call: Call<Volume?>, response: Response<Volume?>) {
                         response.body()?.let {
-                            recyclerView.adapter = GoogleBookAdapter(it.items)
+                            val data = it.items.map { i -> Book().applyData(i) }
+                            recyclerView.adapter = BookAdapter(data)
                         }
                     }
 
                     override fun onFailure(call: Call<Volume?>, t: Throwable) {
                         Log.d("ERROR GOOGLE BOOK API:", t.message)
-                        Toast.makeText(this@ResultsBooks, "Falha ao consultar dados", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@ResultsBooksActivity, "Falha ao consultar dados", Toast.LENGTH_SHORT).show()
                     }
                 })
         }
     }
 
 
-    private fun setupRecycleView() {
+    private fun setupRecyclerView() {
         // Set Recycle Layout
         recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
